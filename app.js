8693c810bc3c17fc88f847bd3c7810e22c29ae8d
Chetan Shankar <chshankar.213@gmail.com>
2020-10-27 19:20:30 -0400

    feat: added cors

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose')
 const autoIncrement = require('mongoose-plugin-autoinc')
 
 const config = require('config')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
 
 const passport = require('passport')
@@ -14,7 +16,8 @@ const app = express()
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
