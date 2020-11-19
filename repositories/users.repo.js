5fea5c3e1308c886d333467d796b95ec7efb1f3b
Chetan Shankar <chshankar.213@gmail.com>
2020-11-18 23:27:32 -0500

    feat: create api to update user

--- repositories/users.repo.js
+++ repositories/users.repo.js
@@ -14,13 +14,12 @@ module.exports.insertOne = async (documentJSON) => {
 
         return await newUser.save()
     } catch (e) {
-        utils.logger.error("error while saving data to mongo db", e)
-        throw new Error("Unable to add Users")
+        utils.logger.error('error while saving data to mongo db', e)
+        throw new Error('Unable to add Users')
     }
 }
 
 module.exports.findOne = async (findQuery) => {
-
     try {
         const user = await User.findOne(findQuery)
         utils.logger.debug(`user_detail with matching Query ${JSON.stringify(findQuery)}  =  ${user}`)
@@ -31,48 +30,59 @@ module.exports.findOne = async (findQuery) => {
     } catch (e) {
         utils.logger.error(`error while finding data from collection : ${utils.dbCons.COLLECTION_USERS} for query ${findQuery}`, e)
 
-        throw new Error("Error finding users for given query")
+        throw new Error('Error finding users for given query')
     }
 
+}
+
+module.exports.updateUserById = async (userID, dataToUpdate) => {
+    try {
+        utils.logger.debug('User Object Data', dataToUpdate)
+        dataToUpdate[utils.dbCons.COMMON_UPDATED_ON] = Date.now()
+        const updateUserQuery = {}
+        updateUserQuery[utils.dbCons.FIELD_ID] = userID
+        utils.logger.debug(`Query for update ${JSON.stringify(updateUserQuery)}`, `data to update = ${JSON.stringify(dataToUpdate)}`)
+        await User.update(updateUserQuery, dataToUpdate)
+        return User.find(updateUserQuery)
+    } catch (e) {
+        utils.logger.error(`error while updating data to mongo db, collection : ${userCollection} for user id ${userID}`, e)
+        return new Error('Invalid Data')
+    }
 }
 
 module.exports.update = async (query, dataToUpdate) => {
     try {
         utils.logger.debug(`data to be updated : ${JSON.stringify(dataToUpdate)}`, `update query : ${JSON.stringify(query)}`)
 
-        const result = await User.findOneAndUpdate(query, {$set: dataToUpdate}, {new: true})
-        utils.logger.debug("Users Object Data", result)
+        const result = await User.findOneAndUpdate(query, { $set: dataToUpdate }, { new: true })
+        utils.logger.debug('Users Object Data', result)
         return result
     } catch (e) {
-        utils.logger.error("Error while updating  data to mongo db", e)
-        throw new Error("Invalid Data")
+        utils.logger.error('Error while updating  data to mongo db', e)
+        throw new Error('Invalid Data')
     }
 }
-
 
 module.exports.updatePassword = async (query, password) => {
     try {
         utils.logger.debug(`Data to be updated : ${JSON.stringify(password)}`, `update query : ${JSON.stringify(query)}`)
-        const user = await  User.findOne(query)
+        const user = await User.findOne(query)
         const newUser = new User(user)
 
         newUser.setPassword(password)
         const dataToUpdate = {
-            hash: newUser.hash,
-            salt: newUser.salt
+            hash: newUser.hash, salt: newUser.salt,
         }
 
-        const result = await User.findOneAndUpdate(query, {$set: dataToUpdate}, {new: true})
-        utils.logger.debug("Users Object Data", result)
+        const result = await User.findOneAndUpdate(query, { $set: dataToUpdate }, { new: true })
+        utils.logger.debug('Users Object Data', result)
         return result
 
     } catch (e) {
-        utils.logger.error("error while updating password data to mongo db", e)
-        throw new Error("Invalid Data")
+        utils.logger.error('error while updating password data to mongo db', e)
+        throw new Error('Invalid Data')
     }
 }
-
-
 
 module.exports.findAll = async (findQuery) => {
     try {
@@ -82,11 +92,9 @@ module.exports.findAll = async (findQuery) => {
         return result
     } catch (e) {
         utils.logger.error(`error while finding data from collection : ${utils.dbCons.COLLECTION_USERS} for query ${findQuery}`, e)
-        throw new Error("Error finding users for given query")
+        throw new Error('Error finding users for given query')
     }
 }
-
-
 
 module.exports.count = async (query) => {
     try {
@@ -96,7 +104,7 @@ module.exports.count = async (query) => {
         return count
     } catch (e) {
         utils.logger.error(`error while fetching count of user from mongo db with query ${JSON.stringify(query)}, collection : ${userCollection} `, e)
-        throw new Error("Error finding count of users for given query")
+        throw new Error('Error finding count of users for given query')
     }
 }
 
